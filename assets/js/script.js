c94edd18d790c848caa4b2bd765c35fafb89382a
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-20 15:58:47 -0400

    fetching all data needed, pseudocode for when and where to create and append elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
 //on click user input as variable
-//fetch from API using city input as query
-//create and append button with saved city in aside
-//create and append current day elements into current day div
-//create and append forecast elements into 5 day div
+$("#search").on("click", function () {
+  var cityInput = $("#city").val().trim();
+
+  //TODO use cityInput for saved button and current forecast h2
+  //same function use moment or whats here to slap the date on there
+
+  //get lon and lat of city, pass it on
+  var requestLocationUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityInput +
+    "&limit=1&appid=cb9916baff495c48795f434351381868";
+  fetch(requestLocationUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getWeatherInfo(lat, lon);
+    });
+
+  function getWeatherInfo(lat, lon) {
+    var requestUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=minutely,hourly&appid=cb9916baff495c48795f434351381868";
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var currentWeather = data.current; //need: temp, clouds, humidity, wind_speed, uvi
+        var dailyWeather = data.daily; //skip 0, for each date, clouds, temp, wind_speed, humidity
+        console.log(currentWeather, dailyWeather);
+      });
+  }
+});
