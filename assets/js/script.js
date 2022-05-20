fb4a53e347248667582f5a9dc37257018a9306fe
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-20 13:47:16 -0400

    add reqirements to readme, pseudocode to html and js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//on click user input as variable
+//fetch from API using city input as query
+//create and append button with saved city in aside
+//create and append current day elements into current day div
+//create and append forecast elements into 5 day div
