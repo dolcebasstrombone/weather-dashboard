0ad17537322725ace7dc33c54a2b4f286fd94f43
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-05-20 19:31:54 -0400

    add functions to display info from fetch responses

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,19 @@
 $("#search").on("click", function () {
   var cityInput = $("#city").val().trim();
 
-  //TODO use cityInput for saved button and current forecast h2
-  //same function use moment or whats here to slap the date on there
+  //create and display city name and date
+  var currentHeaderEl = $("#current-header");
+  currentHeaderEl.text("");
+  var cityNameDateEl = $("<h2>").text(
+    cityInput + " (" + moment().format("MM[/]DD[/]YY") + ")"
+  );
+  currentHeaderEl.append(cityNameDateEl);
+
+  //create a saved city button
+  var savedCitiesContainer = $("#saved-cities");
+  var savedCity = $("<button>").text(cityInput);
+  savedCitiesContainer.append(savedCity);
+  //----------------------------------------------------------------------TODO dont execute above code if the button already exists
 
   //get lon and lat of city, pass it on
   var requestLocationUrl =
@@ -32,9 +43,64 @@ $("#search").on("click", function () {
         return response.json();
       })
       .then(function (data) {
-        var currentWeather = data.current; //need: temp, clouds, humidity, wind_speed, uvi
-        var dailyWeather = data.daily; //skip 0, for each date, clouds, temp, wind_speed, humidity
-        console.log(currentWeather, dailyWeather);
+        var currentData = data.current;
+        var dailyData = data.daily;
+        displayWeather(currentData, dailyData);
       });
+
+    function displayWeather(currentData, dailyData) {
+      //-----------------------------------------------------------------------------TODO use cloud info to choose and display icon
+      //currentCloudEl = $('<li>').text('' + currentData.clouds);
+      //currentInfoEl.append(currentCloudEl);
+
+      //create children for the #current-info ul
+      var currentInfoEl = $("#current-info");
+      currentInfoEl.text("");
+      var currentTempEl = $("<li>").text("Temperature: " + currentData.temp);
+      currentInfoEl.append(currentTempEl);
+      var currentHumidityEl = $("<li>").text(
+        "Humidity: " + currentData.humidity
+      );
+      currentInfoEl.append(currentHumidityEl);
+      var currentWindEl = $("<li>").text("Wind: " + currentData.wind_speed);
+      currentInfoEl.append(currentWindEl);
+      var currentUviEl = $("<li>").text("UV Index: " + currentData.uvi);
+      currentInfoEl.append(currentUviEl);
+      //-------------------------------------------------------------------------------TODO change uvi background to be color coded
+
+      //create the header for the future container
+      $("#future-header").text("5-Day Forecast");
+
+      //console.log(dailyData); //skip 0, for each date, clouds, temp, wind_speed, humidity
+      //create cards for the next 5 days
+      for (var i = 1; i < 6; i++) {
+        dayData = dailyData[i];
+        var cardContainerEl = $("#card-container");
+        var dayCardEl = $("<div>").addClass("card");
+        //add title for date with class card-title
+        var cardTitleEl = $("<h4>")
+          .addClass("card-title")
+          .text(moment().add(i, "days").format("MM[/]DD[/]YY"));
+        dayCardEl.append(cardTitleEl);
+        //---------------------------------------------------------------------------------------------TODO add icon based on cloud
+        //add card text for temp with class card-text
+        var dayTempEl = $("<p>")
+          .addClass("card-text")
+          .text("Temp: " + dayData.temp.day);
+        dayCardEl.append(dayTempEl);
+        //add card text for wind_speed with class card-text
+        var dayWindEl = $("<p>")
+          .addClass("card-text")
+          .text("Wind: " + dayData.wind_speed);
+        dayCardEl.append(dayWindEl);
+        //add card text for humidity with class card-text
+        var dayHumidityEl = $("<p>")
+          .addClass("card-text")
+          .text("Humidity: " + dayData.humidity);
+        dayCardEl.append(dayHumidityEl);
+        //append the whole card
+        cardContainerEl.append(dayCardEl);
+      }
+    }
   }
 });
